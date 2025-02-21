73d8b5b037c86e0cef9dd8da245d74bd6b136081
John J. Meza <johnometalmanldu@gmail.com>
2025-02-21 14:43:46 -0500

    test selenium

---
+++ py-files/test_env.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+print("NOTION_TOKEN:", os.getenv("NOTION_TOKEN"))
+print("DATABASE_ID:", os.getenv("DATABASE_ID"))
+print("WHATSAPP_GROUP_NAME:", os.getenv("WHATSAPP_GROUP_NAME"))
